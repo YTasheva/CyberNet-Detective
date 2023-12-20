a5ce3186c3c5acadc9159804d3d223901f814522
Enky <enkh6at@yahoo.com>
2023-12-20 20:49:53 +0000

    new branch added varieble

--- assets/scripts/calltracker.js
+++ assets/scripts/calltracker.js
@@ -1,48 +1,24 @@
-// const url = "https://true_caller-api.p.rapidapi.com/lookup/";
-// const options = {
-//   method: "POST",
-//   headers: {
-//     "content-type": "application/json",
-//     "X-RapidAPI-Key": "2a1c572212msh9e5d1cd08c9376ap1d134bjsnceccdc52582f",
-//     "X-RapidAPI-Host": "true_caller-api.p.rapidapi.com",
-//   },
-//   body: JSON.stringify({
-//     number: "1234567890",
-//     country_code: "in",
-//   }),
-// };
-
-// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
-
-
-//
 async function getData() {
-    const url = "https://true_caller-api.p.rapidapi.com/";
-    const options = {
+
+    var number = $("#numberInput").val();
+    var countryCode = $("#numberInput").val();
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://true_caller-api.p.rapidapi.com/",
       method: "POST",
       headers: {
         "content-type": "application/json",
-        "X-RapidAPI-Key": "2a1c572212msh9e5d1cd08c9376ap1d134bjsnceccdc52582f",
+        "X-RapidAPI-Key": "e836cf5203msh52715a7d81a978ap1eb4a7jsne7d2dd82308e",
         "X-RapidAPI-Host": "true_caller-api.p.rapidapi.com",
       },
-      body: {
-        number: "1234567890",
-        country_code: "in",
-      },
+      processData: false,
+      data: '{\n    "number":' + String(number) + ',\n    "country_code": '+ String(countryCode) +'\n}',
     };
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
 }
 
 getData();
