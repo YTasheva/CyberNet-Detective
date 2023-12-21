2b6c4e7d55eea075a0140d00b91a2fd5de03b088
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-21 21:58:40 +0000

    added a modal to the html page, added validation for terms and agreements, if pressing save on modal changes terms to true and saves that to local storage so user no longer needs to be validated again

--- assets/scripts/iptracker.js
+++ assets/scripts/iptracker.js
@@ -1,15 +1,30 @@
-
+var terms = false
 $(function () {
+    getTerms();
+    
+    $('#saveTerms').on('click', function(event) {
+        event.preventDefault();
+        terms = true
+        localStorage.setItem("terms", JSON.stringify(terms));
+    });
+
     $('#search-button').on('click', function (event) {
         event.preventDefault();
-        var ip = $('#search-input').val();
-        var ipaddress =  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/; 
-        if (ipaddress.test(ip)) {
-            getIP(ip);
-        }
-        else {
-            $('#trueValue').text('Not a real IP address');
-    
+        if (terms == false) {
+            $('#terms').modal('show');
+        } else {
+            
+            var ip = $('#search-input').val();
+            var ipaddress = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+
+
+            if (ipaddress.test(ip)) {
+                getIP(ip);
+            }
+            else {
+                $('#trueValue').text('Not a real IP address');
+
+            }
         }
     });
 
@@ -30,24 +45,32 @@ function getIP(ip) {
 
     $.ajax(settings).done(function (response) {
         console.log(response);
-        
-            /* Loop through response result */
-            jQuery.each(response.result, function (title, value) {
-                var p = $('<p>').text(title + ": ");
-                if (value == true) {
-                    var icon = $('<img>').attr('src', 'assets/Images/icons8-check-50.png');
-                } else {
-                    var icon = $('<img>').attr('src', 'assets/Images/icons8-cross-50.png');
-                }
-                p.append(icon)
-                $(".map-container").append(p);
-                return
-            });
-        
+
+        /* Loop through response result */
+        jQuery.each(response.result, function (title, value) {
+            var p = $('<p>').text(title + ": ");
+            if (value == true) {
+                var icon = $('<img>').attr('src', 'assets/Images/icons8-check-50.png');
+            } else {
+                var icon = $('<img>').attr('src', 'assets/Images/icons8-cross-50.png');
+            }
+            p.append(icon)
+            $(".map-container").append(p);
+            return
+        });
+
 
     });
 
 
+}
+
+
+function getTerms() {
+    var getTerms = JSON.parse(localStorage.getItem("terms"));
+    if (getTerms !== null) {
+        terms = getTerms;
+    }
 }
 
 function old() {
