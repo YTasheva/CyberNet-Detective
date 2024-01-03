6947b1b49647c38e2b45545b398d48418303fd63
Enky <enkh6at@yahoo.com>
2024-01-03 01:52:33 +0000

    search button and reset button

--- assets/scripts/calltracker.js
+++ assets/scripts/calltracker.js
@@ -15,25 +15,24 @@ async function getData() {
     };
 
     $.ajax(settings).done(function (response) {
-      console.log(response.timestamp, response.status, response);
+      // console.log(response.timestamp, response.data[0].spamInfo, response);
+      var data = response.data[0].spamInfo.spamType
+      localStorage.setItem("spam-info", data)
+      var storedData = localStorage.getItem("spam-info")
+      $(".searh-list").text(storedData)
     });
   });
 }
-
+$(("#reset-button")).click(function () {
+  localStorage.clear();
+});
 getData();
 
 // key1 = e836cf5203msh52715a7d81a978ap1eb4a7jsne7d2dd82308e;
 // key2 = a4a9148c55mshd51a0247b343d9ep18b62ejsn9b26d87bdecb
 // key3 = 6ddaa51237mshe5dca3f57aaf9eap1694c4jsnc5f95fdfc5c3
 // key4 = 2a1c572212msh9e5d1cd08c9376ap1d134bjsnceccdc52582f
-// const iso3166 = require('iso-3166-1-alpha-2');
 
-// // Get the country name by code
-// const countryName = iso3166.getCountry('US');
-// console.log(countryName);  // Output: United States
 
-// // Get the country code by name
-// const countryCode = iso3166.getCode('United States');
-// console.log(countryCode);  // Output: US
 // key1 = a4a9148c55mshd51a0247b343d9ep18b62ejsn9b26d87bdecb
 // key2 = 6ddaa51237mshe5dca3f57aaf9eap1694c4jsnc5f95fdfc5c3
