2ed8316adfb5914762cc2886acc3a3f3f418918a
Enky <enkh6at@yahoo.com>
2024-01-04 13:58:19 +0000

    added style upon scam or not

--- assets/scripts/calltracker.js
+++ assets/scripts/calltracker.js
@@ -9,31 +9,30 @@ async function getData() {
       url: `https://truecaller4.p.rapidapi.com/api/v1/getDetails?phone=${number}&countryCode=${countryCode}`,
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "a4a9148c55mshd51a0247b343d9ep18b62ejsn9b26d87bdecb",
+        "X-RapidAPI-Key": "e836cf5203msh52715a7d81a978ap1eb4a7jsne7d2dd82308e",
         "X-RapidAPI-Host": "truecaller4.p.rapidapi.com",
       },
     };
 
     $.ajax(settings).done(function (response) {
-      // console.log(response.timestamp, response.data[0].spamInfo, response);
-      var data = response.data[0].spamInfo.spamType
-      localStorage.setItem("spam-info", data)
-      var storedData = localStorage.getItem("spam-info")
-      $(".search-list").text(storedData)
+      var data = response.data[0].spamInfo.spamType;
+      
+
+        if (data === "TOP_SPAMMER") {
+          $(".search-list").css("color", "red");
+        } else if (data === false || data === "" || data === null) {
+          $(".search-list").css("color", "green");
+        }
+
+      localStorage.setItem("spam-info", data);
+      var storedData = localStorage.getItem("spam-info");
+      $(".search-list").text(storedData);
     });
   });
 }
-$(("#reset-button")).click(function () {
-  localStorage.clear();
+$("#reset-button").on("click", function () {
+  $(".search-list").empty()
 });
 getData();
 
 
-// key1 = e836cf5203msh52715a7d81a978ap1eb4a7jsne7d2dd82308e;
-// key2 = a4a9148c55mshd51a0247b343d9ep18b62ejsn9b26d87bdecb
-// key3 = 6ddaa51237mshe5dca3f57aaf9eap1694c4jsnc5f95fdfc5c3
-// key4 = 2a1c572212msh9e5d1cd08c9376ap1d134bjsnceccdc52582f
-
-
-// key1 = a4a9148c55mshd51a0247b343d9ep18b62ejsn9b26d87bdecb
-// key2 = 6ddaa51237mshe5dca3f57aaf9eap1694c4jsnc5f95fdfc5c3
